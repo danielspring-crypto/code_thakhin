939c7dcf069e76d2be12cb798c98f12e8216fd74
danielspring-crypto <borncode3@gmail.com>
2020-12-06 13:35:01 +0630

    second commit

--- codeapp/urls.py
+++ codeapp/urls.py
@@ -5,6 +5,6 @@ from .views import LikeView
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('<int:python_id>/', views.detail, name='detail'),
-	path('about/', views.about, name='about'),
 	path('like/<int:pk>/', LikeView, name='like_python'),
+	path('about/', views.about, name='about'),
 ]
--- codeapp/views.py
+++ codeapp/views.py
@@ -6,6 +6,9 @@ from django.urls import reverse
 # Create your views here.
 from .models import Python
 
+def about(request):
+	return render(request, 'codeapp/about.html')
+
 def LikeView(request, pk):
 	python = get_object_or_404(Python, id=request.POST.get('python_id'))
 	python.likes.add(request.user)
@@ -15,12 +18,9 @@ def index(request):
 	pythons = Python.objects.all()
 	return render(request, 'codeapp/index.html', {'pythons':pythons})
 
-
 def detail(request, python_id):
 	python = get_object_or_404(Python, pk=python_id)
 	stuff = get_object_or_404(Python, id=python_id)
 	total_likes = stuff.total_likes()
 	return render(request, 'codeapp/detail.html', {'python':python, 'total_likes':total_likes})
-
-def about(request):
-	return render(request, 'codeapp/about.html')
+	
